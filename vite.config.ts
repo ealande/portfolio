326d73f865adabf7c7d430aaadf0a06af2df2f33
ealande <erickalan23@gmail.com>
2024-09-23 08:57:46 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 443, // Set the port to 443
-    https: true, // Ensure HTTPS is used
-  },
-});
-
+})
