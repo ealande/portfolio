6a55239edfdf2da282bc438f262d15327506c515
ealande <erickalan23@gmail.com>
2024-09-23 09:34:16 -0300

    fixing deploy problems

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  //change port for production
   plugins: [react()],
-})
+  preview: {
+    port: 443,
+  },
+  // for dev
+  server: {
+    port: 3000,
+  },
+});
